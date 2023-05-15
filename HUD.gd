a6846b2c250fa7ad8a68dbcda8c173e4e7b05694
riddled1002 <130505545+riddled1002@users.noreply.github.com>
2023-05-15 17:31:33 -0400

    modified:   HUD.gd 	modified:   main.gd 	modified:   main.tscn
    Added HUD scene to main scene.

--- HUD.gd
+++ HUD.gd
@@ -30,11 +30,4 @@ func _on_message_timeout():
 	$Message.hide()
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
