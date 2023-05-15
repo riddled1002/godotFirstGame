bb5f19e6314ac8cce07101b94ba5cd9d235fbfbe
riddled1002 <130505545+riddled1002@users.noreply.github.com>
2023-05-15 17:03:20 -0400

    modified:   main.gd

--- main.gd
+++ main.gd
@@ -6,7 +6,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
