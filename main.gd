ff5bbbee1a87baffaa3bb2de360e9f37ed0d06ee
riddled1002 <cdbush1002@gmail.com>
2023-05-17 11:59:44 -0400

    modified:   main.gd Tried to get game to start.

--- main.gd
+++ main.gd
@@ -6,7 +6,8 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	if StartButton.pressed:
+		new_game()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
