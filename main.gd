efcaf9c3a2bb44a2f3a42a167100726d02ae7588
riddled1002 <cdbush1002@gmail.com>
2023-05-17 12:01:31 -0400

    modified:   main.gd
    Removed excess code.

--- main.gd
+++ main.gd
@@ -3,18 +3,6 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	if StartButton.pressed:
-		new_game()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
-
-
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
